bf47fd6d88d368f365e051ab855d0cb02414d895
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-13 12:53:46 -0400

    worked on the handleEqual function

--- src/components/CalcBody.js
+++ src/components/CalcBody.js
@@ -11,13 +11,49 @@ export default function CalcBody() {
   const [state, setState] = useState('');
 
   const handleButtonClick = (event) => {
-    // const currentState = state
     const newState = state.concat(event.target.value);
     console.log(newState);
     setState(newState);
   };
 
-  const handleEqual = (event) => {}
+  const handleEqual = (event) => {
+      const stateString = state;
+      const splitStr = stateString.split(" ");
+      console.log(splitStr);
+      if (splitStr.length > 3) {
+          alert('oops! This is a dumb calculator- You can only perform one operation on 2 numbers at a time')
+      }
+      const num1 = parseInt(splitStr[0]);
+      const num2 = parseInt(splitStr[2]);
+      if (splitStr[1] === "+") {
+          const sum = num1 + num2;
+          console.log(sum)
+          if (isNaN(sum)) {
+              alert('Oops! I think you may have a typo!')
+          }
+      } else if (splitStr[1] === "-") {
+          const difference = num1 - num2;
+          console.log(difference);
+          if (isNaN(difference)) {
+            alert('Oops! I think you may have a typo!')
+        }
+      } else if (splitStr[1] === "*") {
+          const product = num1 * num2;
+          console.log(product);
+          if (isNaN(product)) {
+            alert('Oops! I think you may have a typo!')
+        }
+      } else if (splitStr[1] === "/") {
+          const quotient = num1 / num2;
+          console.log(quotient);
+          if (isNaN(quotient)) {
+            alert('Oops! I think you may have a typo!')
+        }
+      } else {
+          alert("Hmmm, something went wrong...Check what you put into the calculator and try again.")
+      }
+      
+  }
 
   const handleClear = (event) => {
       setState('');
@@ -41,7 +77,7 @@ export default function CalcBody() {
           <NumButton value={9} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={'/'} handleButtonClick={handleButtonClick} />
+          <OperationButton operation={' / '} handleButtonClick={handleButtonClick} />
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -55,7 +91,7 @@ export default function CalcBody() {
           <NumButton value={6} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={'*'} handleButtonClick={handleButtonClick}/>
+          <OperationButton operation={' * '} handleButtonClick={handleButtonClick}/>
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -69,7 +105,7 @@ export default function CalcBody() {
           <NumButton value={3} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={'-'} handleButtonClick={handleButtonClick}/>
+          <OperationButton operation={' - '} handleButtonClick={handleButtonClick}/>
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -80,10 +116,10 @@ export default function CalcBody() {
           <NumButton value={0} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <EqualButton />
+          <EqualButton handleEqual={handleEqual}/>
         </Col>
         <Col xs={3}>
-          <OperationButton operation={'+'} handleButtonClick={handleButtonClick} />
+          <OperationButton operation={' + '} handleButtonClick={handleButtonClick} />
         </Col>
       </Row>
     </Container>
--- src/components/EqualButton.js
+++ src/components/EqualButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 
-export default function EqualButton() {
-  return <Button variant="outline-success">Equal</Button>;
+export default function EqualButton(props) {
+  return <Button variant="outline-success" onClick={props.handleEqual} >Equal</Button>;
 }
