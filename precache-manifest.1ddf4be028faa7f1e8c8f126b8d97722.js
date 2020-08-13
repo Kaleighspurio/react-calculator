34121b93205602f239343ebdd8ba62686bbbb34c
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-13 14:18:56 -0400

    Updates

---
+++ precache-manifest.1ddf4be028faa7f1e8c8f126b8d97722.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0674b21610b8019d1ac51d900c22eefd",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "c6b119dab10e1620be43",
+    "url": "/react-calculator/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "38d726178d687aa37417",
+    "url": "/react-calculator/static/css/main.4a11a8e0.chunk.css"
+  },
+  {
+    "revision": "c6b119dab10e1620be43",
+    "url": "/react-calculator/static/js/2.023be3e8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-calculator/static/js/2.023be3e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d726178d687aa37417",
+    "url": "/react-calculator/static/js/main.96b08c32.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
