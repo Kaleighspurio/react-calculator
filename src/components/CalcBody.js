28386e80b0d0e01e9bb6a67946a4430bb53c3003
Kaleigh Spurio <kaleighastell@gmail.com>
2020-08-13 13:02:46 -0400

    finished handleEqual function

--- src/components/CalcBody.js
+++ src/components/CalcBody.js
@@ -17,47 +17,60 @@ export default function CalcBody() {
   };
 
   const handleEqual = (event) => {
-      const stateString = state;
-      const splitStr = stateString.split(" ");
-      console.log(splitStr);
-      if (splitStr.length > 3) {
-          alert('oops! This is a dumb calculator- You can only perform one operation on 2 numbers at a time')
+    const stateString = state;
+    const splitStr = stateString.split(' ');
+    console.log(splitStr);
+    if (splitStr.length > 3) {
+      alert(
+        'oops! This is a dumb calculator- You can only perform one operation on 2 numbers at a time'
+      );
+      return;
+    }
+    const num1 = parseInt(splitStr[0]);
+    const num2 = parseInt(splitStr[2]);
+    if (splitStr[1] === '+') {
+      const sum = num1 + num2;
+      console.log(sum);
+      if (isNaN(sum)) {
+        alert('Oops! I think you may have a typo!');
+        return;
       }
-      const num1 = parseInt(splitStr[0]);
-      const num2 = parseInt(splitStr[2]);
-      if (splitStr[1] === "+") {
-          const sum = num1 + num2;
-          console.log(sum)
-          if (isNaN(sum)) {
-              alert('Oops! I think you may have a typo!')
-          }
-      } else if (splitStr[1] === "-") {
-          const difference = num1 - num2;
-          console.log(difference);
-          if (isNaN(difference)) {
-            alert('Oops! I think you may have a typo!')
-        }
-      } else if (splitStr[1] === "*") {
-          const product = num1 * num2;
-          console.log(product);
-          if (isNaN(product)) {
-            alert('Oops! I think you may have a typo!')
-        }
-      } else if (splitStr[1] === "/") {
-          const quotient = num1 / num2;
-          console.log(quotient);
-          if (isNaN(quotient)) {
-            alert('Oops! I think you may have a typo!')
-        }
-      } else {
-          alert("Hmmm, something went wrong...Check what you put into the calculator and try again.")
+      const answerState = state.concat(` = ${sum}`);
+      setState(answerState);
+    } else if (splitStr[1] === '-') {
+      const difference = num1 - num2;
+      console.log(difference);
+      if (isNaN(difference)) {
+        alert('Oops! I think you may have a typo!');
       }
-      
-  }
+      const answerState = state.concat(` = ${difference}`);
+      setState(answerState);
+    } else if (splitStr[1] === '*') {
+      const product = num1 * num2;
+      console.log(product);
+      if (isNaN(product)) {
+        alert('Oops! I think you may have a typo!');
+      }
+      const answerState = state.concat(` = ${product}`);
+      setState(answerState);
+    } else if (splitStr[1] === '/') {
+      const quotient = num1 / num2;
+      console.log(quotient);
+      if (isNaN(quotient)) {
+        alert('Oops! I think you may have a typo!');
+      }
+      const answerState = state.concat(` = ${quotient}`);
+      setState(answerState);
+    } else {
+      alert(
+        'Hmmm, something went wrong...Check what you put into the calculator and try again.'
+      );
+    }
+  };
 
   const handleClear = (event) => {
-      setState('');
-  }
+    setState('');
+  };
 
   return (
     <Container>
@@ -77,7 +90,10 @@ export default function CalcBody() {
           <NumButton value={9} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={' / '} handleButtonClick={handleButtonClick} />
+          <OperationButton
+            operation={' / '}
+            handleButtonClick={handleButtonClick}
+          />
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -91,7 +107,10 @@ export default function CalcBody() {
           <NumButton value={6} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={' * '} handleButtonClick={handleButtonClick}/>
+          <OperationButton
+            operation={' * '}
+            handleButtonClick={handleButtonClick}
+          />
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -105,7 +124,10 @@ export default function CalcBody() {
           <NumButton value={3} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={' - '} handleButtonClick={handleButtonClick}/>
+          <OperationButton
+            operation={' - '}
+            handleButtonClick={handleButtonClick}
+          />
         </Col>
       </Row>
       <Row xs={2} md={4} lg={6} className="justify-content-center">
@@ -116,10 +138,13 @@ export default function CalcBody() {
           <NumButton value={0} handleButtonClick={handleButtonClick} />
         </Col>
         <Col xs={3}>
-          <EqualButton handleEqual={handleEqual}/>
+          <EqualButton handleEqual={handleEqual} />
         </Col>
         <Col xs={3}>
-          <OperationButton operation={' + '} handleButtonClick={handleButtonClick} />
+          <OperationButton
+            operation={' + '}
+            handleButtonClick={handleButtonClick}
+          />
         </Col>
       </Row>
     </Container>
